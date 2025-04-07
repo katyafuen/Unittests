bd9b42fc5e59cd905f86a7d3b1260bb0991c2f37
Katya <katerynarzh@gmail.com>
2025-04-07 11:56:05 -0400

    tests completed

---
+++ script.js
@@ -0,0 +1,20 @@
+// Unit Tests:
+// Task 1: "multiplication" function.
+// a. Expect multiplication(x, y) to be a number
+// b. Expect the result to be a product of x & y
+
+// Task 2: A "concatOdds" function.
+// a. Expect to return a single array
+// b. Expect the array to contain only odd numbers.
+// c. Expect the array to return the numbers in an ascending order.
+// d. Expect to show an error if got anything other than integers in the array.
+// e. Expect to show an error if got more than 1 array or not an erray.
+// f. Show only 1 of a kind if an integer repeats more than once in the returned array.
+
+// Functional Tests:
+// Task 1: A shopping cart checkout feature
+// a. When a user clicks "Checkout" or "View Shopping Bag", they should be taken to their checkout page, then prompted to choose whether they want to login to their account or checkout as a guest.
+// b. When a user clicks "Login to my account", suggest logining in with email or SSO via gmail or X.
+// c. When a user clicks "Checkout as a guest", skip the login and show the input fields for name, shipping / billing address, email, phone number, and card info.
+// d. Show an error & don't proceed to the next screen if the required fields weren't filled out or filled our with unexpected values, ex email doesn't have '@gmail.com'.
+// Show an error if the card type is not equal to Mastercard, Visa, or AMEX.
